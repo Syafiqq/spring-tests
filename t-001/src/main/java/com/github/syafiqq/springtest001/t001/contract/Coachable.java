cb27ebb816b5bf40fa38986bb81dd62e232786ef
Syafiq <syafiq.rezpector@gmail.com>
2019-06-04 11:59:51 +0700

    Add coachable

---
+++ t-001/src/main/java/com/github/syafiqq/springtest001/t001/contract/Coachable.java
@@ -0,0 +1,13 @@
+package com.github.syafiqq.springtest001.t001.contract;
+
+/*
+ * This <spring-test-001> created by :
+ * Name         : syafiq
+ * Date / Time  : 04 June 2019, 11:52 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public interface Coachable
+{
+    void getDailyWorkout();
+}
